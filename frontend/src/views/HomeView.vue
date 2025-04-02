92a29ee50a4b59e201fb3c37f756ab91b64bca64
Sirapob PHASUT <omegaooh@gmail.com>
2025-04-02 17:48:54 +0700

    Install tailwind and remove unused element

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class='text-center'> Hello! </p>
   </main>
 </template>
