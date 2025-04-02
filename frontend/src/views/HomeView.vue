3764af15b7a27fbbe5e15c197b6a3df647510635
Sirapob PHASUT <omegaooh@gmail.com>
2025-04-02 18:34:23 +0700

    Create 1 mockup for ExploreView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,12 @@
 
 <template>
   <main>
-    <p class='text-center'> Hello! </p>
+    <div class="flex flex-col items-center justify-center h-full">
+      <h1 class="text-4xl font-bold mb-4">Welcome to Traffic Flow Analysis</h1>
+      <p class="text-center">Explore the traffic flow data</p>
+    </div>
+    <div class="flex flex-row justify-center gap-4">
+      
+    </div>
   </main>
 </template>
