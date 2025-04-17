01287d0b736433eb8db6e45a3ca38f40f6c75674
Sirapob PHASUT <omegaooh@gmail.com>
2025-04-17 22:06:09 +0700

    Remove Unnecessary testcase in Frontend

--- frontend/src/lib/test/time_utils.test.js
+++ frontend/src/lib/test/time_utils.test.js
@@ -27,14 +27,6 @@ test("TimeToRFC3339 at 00:45", () => {
     `${now.getFullYear()}-${month}-${day}T01:00:00+07:00`);
 })
 
-test("TimeToRFC3339 at noon", () => {
-  const now = new Date()
-  const month =(now.getMonth()+1).toString().padStart(2, '0')
-  const day =now.getDate().toString().padStart(2, '0')
-  expect(timeToRFC3339("12:00")).toBe(
-    `${now.getFullYear()}-${month}-${day}T12:00:00+07:00`);
-})
-
 test("TimeToRFC3339 with negative hours",() => {
   try{
     timeToRFC3339("-1:00")
